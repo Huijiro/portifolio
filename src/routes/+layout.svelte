daf914776ccfd466788ae3eb72bf2ed75612c56d
Huijiro <huijirohankei@gmail.com>
2024-04-07 01:05:21 -0300

    feat: added meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,30 @@
 
 <svelte:head>
 	<title>Huijiro's Portifolio</title>
-  <meta name="application-name" content="Huijiro's Portifolio" />
-  <meta name="generator" content="Svelte" />
-  <meta name="color-scheme" content="dark light" />
-	<meta name="description" content="Portifolio for WebDeveloper"/>
+	<meta name="application-name" content="Huijiro's Portifolio" />
+	<meta name="generator" content="Svelte" />
+	<meta name="color-scheme" content="dark light" />
+	<meta name="description" content="Portifolio for WebDeveloper" />
 	<meta
 		name="keywords"
 		content="Web Developer, Frontend Developer, Backend Developer, Fullstack Developer, Svelte, Sapper, SvelteKit, React, Next.js, JavaScript, TypeScript, HTML, CSS, TailwindCSS, SASS, SCSS, Node.js, Express, MongoDB, MySQL, PostgreSQL, Firebase, AWS, Azure, Google Cloud, Netlify, Vercel, Heroku, GitHub, GitLab, Bitbucket, Jira, Trello, Asana, Slack, Discord, Zoom, Microsoft Teams, Google Meet, WebRTC, WebSockets, REST, GraphQL, Webhooks, OAuth, JWT, PWA, SPA, SSR, SSG, SEO, Accessibility, Performance, Security, Responsive Design, Mobile First, Dark Mode, Light Mode, Internationalization, Localization, A/B Testing, Analytics, Testing, CI/CD, Docker, Kubernetes, Serverless, Microservices, Monolith, Jamstack, MERN, MEAN, MEVN, LAMP, WAMP, JAMstack, PERN, PERNstack, SvelteKit, Sapper, SvelteKit, React, Next.js, JavaScript, TypeScript, HTML, CSS, TailwindCSS, SASS, SCSS, Node.js, Express, MongoDB, MySQL, PostgreSQL, Firebase, AWS, Azure, Google Cloud, Netlify, Vercel, Heroku, GitHub, GitLab, Bitbucket, Jira, Trello, Asana, Slack, Discord, Zoom, Microsoft Teams, Google Meet, WebRTC, WebSockets, REST, GraphQL, Webhooks, OAuth, JWT, PWA, SPA, SSR, SSG, SEO, Accessibility, Performance, Security, Responsive Design, Mobile First, Dark Mode, Light Mode, Internationalization, Localization, A/B Testing, Analytics, Testing, CI/CD, Docker, Kubernetes, Serverless, Microservices, Monolith, Jamstack, MERN, MEAN, MEVN, LAMP, WAMP, JAMstack, PERN, PERNstack"
 	/>
 	<meta name="author" content="Huijiro" />
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://www.huijiro.com/" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Huijiro's Portifolio" />
+	<meta property="og:description" content="Portifolio for WebDeveloper" />
+	<meta property="og:image" content="" />
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content="huijiro.com" />
+	<meta property="twitter:url" content="https://www.huijiro.com/" />
+	<meta name="twitter:title" content="Huijiro's Portifolio" />
+	<meta name="twitter:description" content="Portifolio for WebDeveloper" />
+	<meta name="twitter:image" content="" />
 </svelte:head>
 
 <div
